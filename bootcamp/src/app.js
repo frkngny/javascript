03a385e41d92b4baadf7dc2aa903dbb76b119bc7
Furkan Guney <furkan.qny@gmail.com>
2021-10-24 22:14:17 +0300

    181021 Ders Sonu

--- bootcamp/src/app.js
+++ bootcamp/src/app.js
@@ -1,1 +1,22 @@
-var asd = 5
+console.log("Hello Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.10
+}
+
+const euroDun = 11.2 // sabittir, sonradan değişmez.
+
+console.log(dolarDun)
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi", "Özel Konut Kredisi"]
+console.log(konutKredileri)
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    const element = konutKredileri[i];
+    console.log("<li>"+element+"</li>")
+}
+console.log("</ul>")
